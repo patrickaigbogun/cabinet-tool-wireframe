1cd450cd5f817fb489c7adaa42d1d01eddc5062f
youngboypimpin <rjtekdom@gmail.com>
2024-09-29 13:26:37 +0100

    starter template for the cabinet tool

---
+++ components/header.tsx
@@ -0,0 +1,18 @@
+
+
+function Header(){
+ return(
+   <header>
+    <div className=" flex flex-row justify-between" >
+        <div>
+            Cabinet Tool
+        </div>
+        <div>
+            <img className="rounded-full" src="#" alt="profile image"  />
+        </div>
+    </div>
+   </header>
+ );
+};
+
+export default Header
