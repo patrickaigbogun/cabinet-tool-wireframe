1431efa57334627dfef1076c70d73c1023fffb7b
youngboypimpin <rjtekdom@gmail.com>
2024-10-06 16:04:19 +0100

    (fix):set footer to fixed bottom

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className=" text-sm bg-slate-900 flex justify-center p-3">
+        <footer className=" w-full bottom-0 fixed text-sm bg-slate-900 flex justify-center p-3">
             <section className=" flex flex-col sm:flex-row sm:justify-center gap-4 items-center max-w-[90%]">
                 <p>System Status | Privacy Policy | Terms & Conditions</p>
                 <p>Copyright © 2024 Cabinet Tool. All rights reserved.</p>
