fd6b133f3d17b2a9818a65c038351afb046a8529
youngboypimpin <rjtekdom@gmail.com>
2024-10-06 19:05:27 +0100

    minor updates

--- components/login.tsx
+++ components/login.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
 	return (
 		<div className="grid grid-cols-2 h-screen bg-gray-400 ">
-			<section className="space-y-6 bg-slate-500 h-full flex flex-col justify-center p-6 ">
+			<section className="space-y-6 bg-slate-700 h-full flex flex-col justify-center p-6 ">
 				<h2 className="font-bold text-5xl">
 					Sign Up Now to Access Your Cabinet
 				</h2>
@@ -11,7 +11,8 @@ export default function Login() {
 			</section>
 			<section className="flex justify-center items-center bg-black p-6">
 				<form className="flex  items-center justify-center  w-full h-full" action="" method="post">
-					<div className="flex flex-col w-full m-5 space-y-4 ">
+					<div className="flex flex-col w-full m-5 space-y-9 ">
+						<div className="space-y-4" >
 						<div className="flex flex-col" >
 							<label htmlFor="username">USERNAME</label>
 							<input className="ring-2 ring-slate-600 rounded-xl" type="text" name="username" id="username" />
@@ -24,7 +25,8 @@ export default function Login() {
 							<label htmlFor="password">PASSWORD</label>
 							<input className="ring-2 ring-slate-600 rounded-xl" type="password" name="password" id="password" />
 						</div>
-						<button type="submit" className="mt-4 bg-slate-600 text-white rounded-xl p-2">
+						</div>
+						<button type="submit" className="mt-4 bg-slate-700 text-white rounded-xl p-2">
 							Sign Up
 						</button>
 					</div>
