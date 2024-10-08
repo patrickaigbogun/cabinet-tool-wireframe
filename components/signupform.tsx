3fcbcc4bff50611b035f5e3eaf43dfa4bcfd0842
youngboypimpin <rjtekdom@gmail.com>
2024-10-08 12:10:28 +0100

    updated the signupform

--- components/signupform.tsx
+++ components/signupform.tsx
@@ -22,29 +22,29 @@ export default function SignupForm() {
 	};
 
 	return (
-		<div className="grid grid-cols-2 h-screen bg-gray-400 ">
-			<section className="space-y-6 bg-slate-700 h-full flex flex-col justify-center p-6 ">
-				<h2 className="font-bold text-5xl">Sign Up Now to Access Your Cabinet</h2>
+		<div className="font-bold grid grid-cols-1 sm:grid-cols-2 h-screen bg-gray-400 ">
+			<section className="space-y-6 bg-slate-700 h-screen sm:h-full flex flex-col justify-center p-6 ">
+				<h2 className="font-extrabold text-5xl">Sign Up Now <br /> and get Access to Your Cabinet</h2>
 				<p>
 					Closed Cabinets should be for your eyes only, create an account with access to just you.
 				</p>
 			</section>
-			<section className="flex justify-center items-center bg-black p-6">
+			<section className="h-screen sm:h-full flex justify-center items-center bg-black p-6">
 				<form className="flex items-center justify-center w-full h-full" onSubmit={handleSignup}>
-					<div className="flex flex-col w-full m-5 space-y-9 ">
+					<div className="flex flex-col w-full m-5 space-y-12 ">
 						{error && <p className="text-red-500">{error}</p>}
-						<div className="space-y-4">
-							<div className="flex flex-col">
-								<label htmlFor="username">USERNAME</label>
-								<input className="ring-2 ring-slate-600 rounded-xl" type="text" name="username" id="username" required />
+						<div className="space-y-6">
+							<div className="flex flex-col gap-y-2">
+								<label className="text-lg" htmlFor="username">USERNAME</label>
+								<input className="ring-2 ring-slate-600 rounded-lg text-black p-3" type="text" name="username" id="username" required />
 							</div>
-							<div className="flex flex-col">
-								<label htmlFor="email">EMAIL</label>
-								<input className="ring-2 ring-slate-600 rounded-xl" type="email" name="email" id="email" required />
+							<div className="flex flex-col gap-y-2">
+								<label className="text-lg" htmlFor="email">EMAIL</label>
+								<input className="ring-2 ring-slate-600 rounded-lg text-black p-3" type="email" name="email" id="email" required />
 							</div>
-							<div className="flex flex-col">
-								<label htmlFor="password">PASSWORD</label>
-								<input className="ring-2 ring-slate-600 rounded-xl text-black" type="password" name="password" id="password" required />
+							<div className="flex flex-col gap-y-2">
+								<label className="text-lg" htmlFor="password">PASSWORD</label>
+								<input className="ring-2 ring-slate-600 rounded-lg text-black p-3" type="password" name="password" id="password" required />
 							</div>
 						</div>
 						<button type="submit" className="mt-4 bg-slate-700 text-white rounded-xl p-2">
