180d6543d7465af7af9c72e99b517fbcadc1acb1
youngboypimpin <rjtekdom@gmail.com>
2024-10-22 18:53:22 +0100

    (fix): build time error due to missing dependency

--- app/closedcabinet/layout.tsx
+++ app/closedcabinet/layout.tsx
@@ -1,19 +1,34 @@
-import type { Metadata } from "next";
-import "../globals.css";
+
+'use client';
+
+// import type { Metadata } from "next";
+import { useEffect } from 'react'
 import '@radix-ui/themes/styles.css';
+import "../globals.css";
 import { Theme } from '@radix-ui/themes';
 import { ThemeProvider } from 'next-themes';
 import CabinetHeader from "@/components/cabinetheader";
+import { validateToken } from "@/actions/auth";
+import { useRouter } from 'next/navigation'
 
-export const metadata: Metadata = {
-	title: "Cabinet Tool",
-	description: "Web storage utility",
-};
-export default function ClosedCabinetLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
+// export const metadata: Metadata = {
+// 	title: "Cabinet Tool",
+// 	description: "Web storage utility",
+// };
+
+
+
+export default function ClosedCabinetLayout({ children }: { children: React.ReactNode }){
+const router = useRouter()
+
+// Check for token on site load
+useEffect(() => {
+	const token = localStorage.getItem("authToken");
+	if (token && validateToken(token)) {
+		router.push('/closedcabinet')
+	}
+}, [router]);
+
 	return (
 		<html lang="en">
 			<body>
